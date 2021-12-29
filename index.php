c089afa0cb644a76e395d4e9f11d9cfbc7f75855
vietanhiptv2 <96271396+vietanhiptv2@users.noreply.github.com>
2021-12-29 16:04:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
+<!DOCTYPE html>
+<html lang=""><head>
+<meta name="description" content="VietAnh TV">
+<link rel="icon" href="http://vietanhiptv.cf/vanhtv.png"><title>VietAnh TV</title>
+<link href="https://monstertv.cf/css/app.fe770523.css" rel="preload" as="style">
+<link href="https://monstertv.cf/js/app.e9f04853.js" rel="preload" as="script">
+<link href="https://monstertv.cf/js/chunk-vendors.86795d31.js" rel="preload" as="script">
+<link href="https://monstertv.cf/css/app.fe770523.css" rel="stylesheet">
+</head><body><div id="app" data-v-app=""><img alt="Va logo" src="http://vietanhiptv.cf/vanhtv.png" width="200" style="margin-top: 20px;"><h1>VietAnh TV</h1><a class="button button1" href="http://vietanhiptv.cf/IPTV_Pro-v6.1.11.apk">IPTV Pro</a><br><br><div>(dành cho điện thoại android)</div><br><br><a class="button button1" href="http://vietanhiptv.cf/MX_Player_Pro_v1.41.1.apk">MXPlayerPro</a><br><br><div>(dành cho điện thoại android)</div><br><br><a class="button button2" href="https://drive.google.com/u/0/uc?export=download&confirm=JNXF&id=11dC73SnJ5nVCOnfPEjZbQ0KYPY45GVez">TiViMate</a><br><br><div>(dành cho tivi android)</div></div>
+</body></html>
 
